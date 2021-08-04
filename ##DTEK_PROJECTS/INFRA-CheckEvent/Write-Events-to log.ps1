15e697800c0036517d7a1174ecce7407c08a40b4
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2021-08-04 15:10:26 +0100

    Update Write-Events-to log.ps1

--- ##DTEK_PROJECTS/INFRA-CheckEvent/Write-Events-to log.ps1
+++ ##DTEK_PROJECTS/INFRA-CheckEvent/Write-Events-to log.ps1
@@ -1,5 +1,5 @@
 #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/write-eventlog?view=powershell-5.1
 
 
-Write-EventLog -LogName "Application" -Source "MyApp" -EventID 3001 -EntryType Information -Message "MyApp added a user-requested feature to the display." -Category 1 -Ra
 
+Write-EventLog -LogName "Application" -Source "MyApp" -EventID 3001 -EntryType Information -Message "MyApp added a user-requested feature to the display." -Category 1 -RawData 10,20
